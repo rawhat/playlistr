0431bde3f583f11a1c0257cf3231a3945584b1f3
Alex Manning <alex41290@gmail.com>
2017-04-28 15:55:43 -0700

    cleaned up files in folder; added testing file for new changes

--- grabber.py
+++
@@ -1,6 +0,0 @@
-import pafy
-from music import Song
-
-def processUrl(url):
-    video = pafy.new(url)
-    return {'title': video.title, 'url': video.getbestaudio().url, 'length': video.length}
---
+++ music-tester.js
@@ -0,0 +1,11 @@
+const { Playlist, Song, PlaylistManager } = require('./music');
+
+let s = new Song("rando", false, "http://www.google.com", 100, "http://www.google.com/1");
+let p = new Playlist("test", "rap", null, null, "audio");
+p.songs.push(s);
+let pm = new PlaylistManager();
+
+module.exports = {
+    p,
+    pm
+}
