0ed09a3b9102ece8984986d8f4c562944ae09bcf
Alex Manning <alex41290@gmail.com>
2017-06-08 22:40:15 -0400

    implemented playlist controller

--- lib/playlistr/web/controllers/playlist_controller.ex
+++ lib/playlistr/web/controllers/playlist_controller.ex
@@ -1,8 +1,9 @@
 defmodule Playlistr.Web.PlaylistController do
     use Playlistr.Web, :controller
 
+    alias Bolt.Sips, as: Bolt
+
     def index(conn, params) do
-        # json conn, %{ ... }
         case params do
             %{ "playlist" => title, "password" => password } ->
                 IO.puts "checking password playlist"
@@ -13,7 +14,7 @@ defmodule Playlistr.Web.PlaylistController do
                     OPTIONAL MATCH (p)-[:HAS]-(s:Song)
                     RETURN p AS playlist, s AS song
                 """
-                case Bolt.Sips.query(Bolt.Sips.conn, cypher) do
+                case Bolt.query(Bolt.conn, cypher) do
                     {:ok, results} ->
                         case results do
                             [] ->
@@ -36,7 +37,7 @@ defmodule Playlistr.Web.PlaylistController do
                     OPTIONAL MATCH (p)-[:HAS]-(s:Song)
                     RETURN p AS playlist, s AS song
                 """
-                case Bolt.Sips.query(Bolt.Sips.conn, cypher) do
+                case Bolt.query(Bolt.conn, cypher) do
                     {:ok, results} ->
                         json conn, (results |> Playlistr.Music.get_playlist)
                 end
@@ -48,7 +49,7 @@ defmodule Playlistr.Web.PlaylistController do
                 """
                 # json conn, %{ :test => "data" }
                 # json conn, %{ :playlists => Playlistr.Music.get_playlists()}
-                case Bolt.Sips.query(Bolt.Sips.conn, cypher) do
+                case Bolt.query(Bolt.conn, cypher) do
                     {:ok, results} ->
                         json conn, (results |> Playlistr.Music.get_playlists)
                     {:err, _} ->
@@ -57,8 +58,54 @@ defmodule Playlistr.Web.PlaylistController do
         end
     end
 
-    def add_playlist(conn, _params) do
-        conn
-        |> send_resp(200, "")
+    def add_playlist(conn, params) do
+        case params do
+            %{ 
+                "title" => title,
+                "category" => category,
+                "password" => password,
+                "openSubmissions" => open_submissions,
+                "type" => type
+            } ->
+                creator = "rawhat"
+                playlist = Playlistr.Playlist.init(title, category, password, open_submissions, type, creator)
+                cypher = """
+                    MATCH (u:User) WHERE u.username = "#{creator}"
+				    CREATE (u)-[:CREATED { createdAt: "#{Playlistr.Music.get_current_epoch_time()}" }]->(p:Playlist {
+                        title: "#{playlist.title}",
+                        category: "#{playlist.category}",
+                        password: "#{playlist.password}",
+                        openSubmissions: "#{playlist.open_submissions}",
+                        type: "#{playlist.type}",
+                        length: "#{playlist.length}",
+                        isPaused: "#{playlist.is_paused}",
+                        startDate: "#{playlist.start_date}",
+                        currentTime: "#{playlist.current_time}",
+                        hasPlayed: "#{playlist.has_played}"
+                    }) RETURN p AS playlist
+                """
+
+                case Bolt.query(Bolt.conn, cypher) do
+                    {:ok, []} ->
+                        conn
+                        |> put_status(400)
+                        |> json(%{ :error => "Playlist not created."})
+
+                    {:ok, _} ->
+                        conn
+                        |> put_status(201)
+                        |> json(%{ :status => "Created" })
+
+                    {:err, _} ->
+                        conn
+                        |> put_status(500)
+                        |> json(%{ :error => "Error.  Database command failed" })
+                end
+
+            _ ->
+                conn
+                |> put_status(400)
+                |> json(%{ :status => "Error.  Invalid parameters" })
+        end
     end
 end
