919a09c22ec3f7fa4b9d58205322f2db6cec5622
alex manning <alex.w.manning@gmail.com>
2017-08-07 23:15:09 -0400

    basic flask setup rd 2, removing elixir stuff

--- api/app.py
+++ api/app.py
@@ -1,6 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, jsonify, render_template
+from flask_socketio import SocketIO
 
-@app.route('/')
+# app = Flask(__name__, static_url_path=os.path.join('../', 'static').replace('\\','/'))
+app = Flask(__name__)
+app.debug = True
+socketio = SocketIO(app)
+
+@app.route('/authenticated')
 def index():
-    return "Hello, world!"
+    return jsonify({})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path>')
+def catch_all(path):
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, None, None, debug=True)
